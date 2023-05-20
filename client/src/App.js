a6617717a84f92428fcd23a20d80788cd96d769b
Syed Salman <syedsalman1006@gmail.com>
2023-05-20 20:03:25 +0530

    Created header and a banner

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Banner from './components/Banner/Banner';
+import GridContainer from './components/Grid-container/Grid-container';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Banner/>
+      <GridContainer/>
     </div>
   );
 }
---
+++ client/src/components/Grid-container/Grid-container.jsx
@@ -0,0 +1,10 @@
+import './Grid-container.css';
+const GridContainer = ()=>{
+    return(
+        <>
+        <section><form action="">todo</form></section>
+        <section className="cards-container"></section>
+        </>
+    )
+}
+export default GridContainer;
